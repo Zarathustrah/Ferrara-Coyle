5a3e7fd0157ca93475ed4fe1deac948b00c921ac
Dan Price <danielparkerprice@gmail.com>
2020-11-30 00:08:24 +0000

    draft

--- ferrara-coyle/src/App.js
+++ ferrara-coyle/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/styles'
+import { Grid, CssBaseline } from '@material-ui/core'
 
+const useStyles = makeStyles({
+  root: {
+    height: '100vh',
+    width: '100vw',
+    background: `url('https://i.ibb.co/Kbm85dy/Ferrara-Coyle-holding-page.jpg')`,
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    
+  }
+})
 
-function App() {
+export default function App() {
+  const classes = useStyles()
+
   return (
-    <div>
-      <p>Edit and save to reload.</p>
-    </div>
-  );
+    <Grid container className={classes.root}>
+      <CssBaseline />
+    </Grid>
+  )
 }
-
-export default App;
